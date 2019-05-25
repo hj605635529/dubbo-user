2349b7a34503b97b30a91f9639565b9b1c48823a
huangjia <jia.huang@qunar.com>
2019-05-25 22:19:37 +0800

    创建项目

---
+++ src/main/java/com.huangjia.dubbo.user/App.java
@@ -0,0 +1,28 @@
+package com.huangjia.dubbo.user;
+
+import java.io.IOException;
+
+/**
+ * @author jia.huang
+ * @date 2019/5/25
+ */
+public class App {
+    public static void main( String[] args ) throws IOException {
+//        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
+//
+//        //用户下单过程
+//        IOrderServices services=(IOrderServices)context.getBean("orderServices");
+//
+//        DoOrderRequest request=new DoOrderRequest();
+//        request.setName("mic");
+//        DoOrderResponse response=services.doOrder(request);
+//
+//        System.out.println(response);
+//
+//        //Order.doOrder();
+//        System.in.read();
+
+        System.out.println("hello world");
+
+    }
+}
