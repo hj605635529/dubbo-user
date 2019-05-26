0dd9a3dea28fd8439b80c6873433ffb8a2b89d5e
huangjia <jia.huang@qunar.com>
2019-05-26 21:03:12 +0800

    项目搭建完成

--- src/main/java/com.huangjia.dubbo.user/App.java
+++ src/main/java/com.huangjia.dubbo.user/App.java
@@ -1,5 +1,10 @@
 package com.huangjia.dubbo.user;
 
+import com.huangjia.dubbo.bean.OrderRequest;
+import com.huangjia.dubbo.bean.OrderResponse;
+import com.huangjia.dubbo.service.OrderServices;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.io.IOException;
 
 /**
@@ -8,19 +13,20 @@ import java.io.IOException;
  */
 public class App {
     public static void main( String[] args ) throws IOException {
-//        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
-//
-//        //用户下单过程
-//        IOrderServices services=(IOrderServices)context.getBean("orderServices");
-//
-//        DoOrderRequest request=new DoOrderRequest();
-//        request.setName("mic");
-//        DoOrderResponse response=services.doOrder(request);
-//
-//        System.out.println(response);
-//
-//        //Order.doOrder();
-//        System.in.read();
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("order-consumer.xml");
+
+        //用户下单过程
+        OrderServices services=(OrderServices)context.getBean("orderServices");
+
+        OrderRequest request=new OrderRequest();
+        request.setName("mic");
+        OrderResponse response=services.doOrder(request);
+
+        System.out.println(response.getCode());
+        System.out.println(response.getMemo());
+
+        //Order.doOrder();
+        System.in.read();
 
         System.out.println("hello world");
 
